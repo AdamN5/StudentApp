0de96ccca99f480a42964335c9c728ac471edbfe
AdamNafea-STUDENT <g00462155@atu.ie>
2025-10-08 17:25:11 +0100

    Added the ability to select number of students and add details for each

--- src/main/java/ie/atu/week3/StudentApp.java
+++ src/main/java/ie/atu/week3/StudentApp.java
@@ -6,10 +6,22 @@ import java.util.List;
 public class StudentApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int count = 0;
 
         System.out.println("Enter the number of students: ");
-
+        int numOfStudents = scanner.nextInt();
+        scanner.nextLine();
+        int i = 0;
+        while (i < numOfStudents) {
+            System.out.println("Enter name of student.");
+            String name = scanner.nextLine();
+            System.out.println("Enter email of student.");
+            String email = scanner.nextLine();
+            System.out.println("Enter ID of Student.");
+            String id = scanner.nextLine();
+            Student student = new Student(name, email, id);
+            System.out.println(student);
+            i++;
+        }
 
     }
 }
